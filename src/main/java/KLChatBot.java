d13c59d493a54d4d04d00805cc6102082f278bf7
khayliang <khayliang@gmail.com>
2026-01-18 17:26:36 +0800

    Add rename greet and exit to bot

---
+++ src/main/java/KLChatBot.java
@@ -0,0 +1,8 @@
+public class KLChatBot {
+    public static void main(String[] args) {
+        String logo = "Hello! I'm KLChatBot,\n" +
+                      "What can I do for you?\n" +
+                      "Bye. Hope to see you again soon!";
+        System.out.println(logo);
+    }
+}
