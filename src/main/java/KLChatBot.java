c762b6cacf5e499373082e65e05fc297749c8262
khayliang <khayliang@gmail.com>
2026-01-18 17:32:45 +0800

    Add echo of user command

--- src/main/java/KLChatBot.java
+++ src/main/java/KLChatBot.java
@@ -1,8 +1,27 @@
+import java.io.BufferedReader;
+
 public class KLChatBot {
     public static void main(String[] args) {
-        String logo = "Hello! I'm KLChatBot,\n" +
-                      "What can I do for you?\n" +
-                      "Bye. Hope to see you again soon!";
-        System.out.println(logo);
+        String opening = "Hello! I'm KLChatBot,\n" +
+                      "What can I do for you?\n";
+        String closing = "\nGoodbye! Have a great day!";
+        System.out.println(opening);
+
+        BufferedReader stdin = new BufferedReader(new java.io.InputStreamReader(System.in));
+
+        while (true) {
+            try {
+                String input = stdin.readLine();
+                if (input.equals("bye")) {
+                    break;
+                }
+                System.out.print("____________________________________________________________\n");
+                System.out.println(input);
+                System.out.print("____________________________________________________________\n");
+            } catch (Exception e) {
+                System.out.println("An error occurred: " + e.getMessage());
+            }
+        }
+        System.out.println(closing);
     }
 }
