ffe4e697d73ce6085b937a7d2b0b3c3b0c0e8608
khayliang <khayliang@gmail.com>
2026-01-18 17:42:39 +0800

    Add task completion feature

--- src/main/java/KLChatBot.java
+++ src/main/java/KLChatBot.java
@@ -10,7 +10,7 @@ public class KLChatBot {
         System.out.println(opening);
 
         BufferedReader stdin = new BufferedReader(new java.io.InputStreamReader(System.in));
-        List<String> userCommands = new ArrayList<>();
+        List<Task> tasks = new ArrayList<>();
 
         while (true) {
             try {
@@ -20,15 +20,35 @@ public class KLChatBot {
                 }
                 if (input.equals("list")) {
                     System.out.print("____________________________________________________________\n");
-                    for (int i = 0; i < userCommands.size(); i++) {
-                        System.out.println(" " + (i + 1) + ". " + userCommands.get(i));
+                    System.out.println(" Here are the tasks in your list:");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println(" " + (i + 1) + "." + tasks.get(i));
+                    }
+                    System.out.print("____________________________________________________________\n");
+                } else if (input.startsWith("mark ")) {
+                    int idx = Integer.parseInt(input.split(" ")[1].trim()) - 1;
+                    if (idx >= 0 && idx < tasks.size()) {
+                        tasks.get(idx).markDone();
+                        System.out.print("____________________________________________________________\n");
+                        System.out.println(" Nice! I've marked this task as done:");
+                        System.out.println("   " + tasks.get(idx));
+                        System.out.print("____________________________________________________________\n");
+                    }
+                } else if (input.startsWith("unmark ")) {
+                    int idx = Integer.parseInt(input.split(" ")[1].trim()) - 1;
+                    if (idx >= 0 && idx < tasks.size()) {
+                        tasks.get(idx).markUndone();
+                        System.out.print("____________________________________________________________\n");
+                        System.out.println(" OK, I've marked this task as not done yet:");
+                        System.out.println("   " + tasks.get(idx));
+                        System.out.print("____________________________________________________________\n");
                     }
                 } else {
-                    userCommands.add(input);
+                    tasks.add(new Task(input));
                     System.out.print("____________________________________________________________\n");
                     System.out.println(" added: " + input);
+                    System.out.print("____________________________________________________________\n");
                 }
-                System.out.print("____________________________________________________________\n");
             } catch (Exception e) {
                 System.out.println("An error occurred: " + e.getMessage());
             }
